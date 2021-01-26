fe8f19a942dabf493b1ece9a7f104a5f20abf38d
Robert Hardy <rwhj77@hotmail.com>
2021-01-25 18:25:36 -0600

    commit tree 7312e4490fe9a2bfbc35e3ff9d2579187ca019da

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,27 @@
 def starts_with_a_vowel?(word)
-
-end
-
-def words_starting_with_un_and_ending_with_ing(text)
-
-end
-
-def words_five_letters_long(text)
-
-end
-
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
-
-def valid_phone_number?(phone)
-
-end
+    if word.match(/\A[aeiouAEIOU]\w+/)
+      return true
+    else
+      return false
+    end
+  end
+  
+  def words_starting_with_un_and_ending_with_ing(text)
+    return text.scan(/[un|Un]\w+ing\b/)
+  end
+  
+  def words_five_letters_long(text)
+    return text.scan(/\b[a-zA-Z]{5}\b/)
+  end
+  
+  def first_word_capitalized_and_ends_with_punctuation?(text)
+    if text.match(/\A[A-Z].+[.!]\z/)
+      return true
+    else
+      return false
+    end
+  end
+  
+  def valid_phone_number?(phone)
+    phone.match(/\D*\d{3}\D*\d{3}\D*\d{4}/)
+  end
